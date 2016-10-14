65e31a0d4f3e640839e5625e5c0751d1a05c963c
Alex Applebaum <a.applebaum@f5.com>
2016-10-14 15:14:41 -0700

    add startup-script

---
+++ templates/app/startup_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+
+# Basic Tools
+sudo apt-get update
+sudo apt-get install -y ntp
+sudo apt-get install -y aptitude
+sudo apt-get install -y git
+sudo apt-get install -y wget
+sudo apt-get install -y tar
+sudo apt-get install -y ca-certificates
+sudo apt-get install -y software-properties-common
+sudo apt-get install -y build-essential
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y python-dev
+sudo apt-get install -y zlib1g-dev
+sudo apt-get install -y libssl-dev
+
+#Install Docker
+echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get update
+sudo apt-get install -y docker-engine
